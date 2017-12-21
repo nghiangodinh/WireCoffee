a2d87dd1dd8e3960f8a49528a7ee76d95cca4534
Nghia Ngo <nghiangodinh@yahoo.com>
2017-12-21 22:46:19 +0700

    add navigation menu

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  menuData = [
+    { title: "Our Menu", pic: "assets/img/soup1.jpg", pushPage: "MenuPage" },
+    {
+      title: "Account",
+      pic: "assets/img/coffee-people3.jpg",
+      pushPage: "AccountPage"
+    },
+    { title: "About Us", pic: "assets/img/coffee6.jpg", pushPage: "AboutPage" },
+    {
+      title: "Locations",
+      pic: "assets/img/cafe2.jpg",
+      pushPage: "LocationsPage"
+    }
+  ];
 
-  constructor(public navCtrl: NavController) {
-
-  }
-
+  constructor(public navCtrl: NavController) {}
 }
